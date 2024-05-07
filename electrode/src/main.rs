12a9f617a0f2e9273fc8a5005e104a04958b36e0
Hennzau <dev@enzo-le-van.fr>
2024-05-07 18:16:31 +0200

    electrode

--- electrode/src/main.rs
+++ electrode/src/main.rs
@@ -1,49 +1,58 @@
-use std::collections::VecDeque;
-use dora_node_api::{DoraNode, Event, EventStream, IntoArrow};
-use std::error::Error;
-use std::ops::DerefMut;
-use std::str::from_utf8;
-use std::sync::{Arc, Mutex};
-use dora_node_api::arrow::array::{Array, UInt8Array};
-use dora_node_api::dora_core::config::DataId;
-use tokio::net::UdpSocket;
+use std::{
+    collections::VecDeque,
+    sync::{
+        Arc,
+        Mutex
+    }
+};
+
+use dora_node_api::{
+    DoraNode,
+    Event,
+    IntoArrow,
+    arrow::array::UInt8Array,
+    dora_core::config::DataId
+};
+
+use simple_logger::SimpleLogger;
 
 struct Packet {
     topic: String,
-    data: Vec<u8>,
+    data: Vec<u8>
 }
 
-async fn run_listener(packets: Arc<Mutex<VecDeque<Packet>>>) {
-    let listener = env!("LISTENER");
+struct Shared {
+    running: bool,
+    packets: VecDeque<Packet>
+}
 
-    let socket = UdpSocket::bind(listener).await.unwrap();
-    let mut buf = [0; 1024 * 1024];
+async fn run_listener(shared: Arc<Mutex<Shared>>) {
+    let args: Vec<String> = std::env::args().collect();
+    let listener = args.get(2).unwrap().clone();
+    log::info!("Electrode Listener: {}", listener);
+
+    let socket = tokio::net::UdpSocket::bind(listener).await.unwrap();
 
     loop {
-        let (len, addr) = socket.recv_from(&mut buf).await.unwrap();
+        // Receive a packet
 
-        let topic_size = buf[0];
-        let topic_utf8 = &buf[1..(1 + topic_size as usize)];
-        let topic_str = from_utf8(&topic_utf8).unwrap().to_string();
+        let shared = shared.lock().unwrap();
 
-        let data = (&buf[(1 + topic_size as usize)..1024 * 1024]).to_vec();
-
-        let mut packets = packets.lock().unwrap();
-        packets.deref_mut().push_back(Packet {
-            topic: topic_str,
-            data,
-        });
+        if !shared.running {
+            break;
+        }
     }
 }
 
 
-async fn run(node: &mut DoraNode, events: &mut EventStream) {
-    let packets: Arc<Mutex<VecDeque<Packet>>> = Arc::new(Mutex::new(VecDeque::new()));
+async fn run(shared: Arc<Mutex<Shared>>) {
+    let (mut node, mut events) = DoraNode::init_from_env().unwrap();
 
-    let listener = run_listener(packets.clone());
+    let args: Vec<String> = std::env::args().collect();
+    let sender = args.get(4).unwrap().clone();
+    log::info!("Electrode Sender: {}", sender);
 
-    let sender = env!("SENDER");
-    let socket = UdpSocket::bind(sender).await.unwrap();
+    let socket = tokio::net::UdpSocket::bind(sender).await.unwrap();
 
     while let Some(event) = events.recv() {
         match event {
@@ -51,43 +60,88 @@ async fn run(node: &mut DoraNode, events: &mut EventStream) {
                 id,
                 metadata,
                 data,
-            } => match id.as_str() {
-                "tick" => {
-                    let mut packets_data = packets.lock().unwrap();
-                    while !packets_data.deref_mut().is_empty() {
-                        let Packet {
-                            topic,
-                            data
-                        } = packets_data.deref_mut().pop_front().unwrap();
+            } => {
+                match id.as_str() {
+                    "tick" => {
+                        // Propagate every packet received from UDPListener into the dataflow
 
-                        node.send_output(
-                            DataId::from(topic),
-                            metadata.parameters.clone(),
-                            data.into_arrow(),
-                        ).unwrap();
+                        let mut shared = shared.lock().unwrap();
+                        while let Some(packet) = shared.packets.pop_front() {
+                            let topic = packet.topic;
+                            let data = packet.data;
 
-                        println! ("data received from udp {}", topic);
+                            let status = node.send_output(
+                                DataId::from(topic),
+                                metadata.parameters.clone(),
+                                data.into_arrow(),
+                            );
+
+                            if let Err(e) = status {
+                                log::error!("Error sending output: {}", e);
+                            }
+                        }
+                    },
+                    other => {
+                        // Send data received from dataflow to UDPListener
+
+                        let buffer: UInt8Array = data.to_data().into();
+                        let data = buffer.values();
+                        let topic = other.as_bytes();
+                        let topic_size = topic.len() as u8;
+
+                        let mut final_data = Vec::new();
+                        final_data.push(topic_size);
+                        final_data.extend_from_slice(topic);
+                        final_data.extend_from_slice(data);
+
+                        let status = socket.send(
+                            final_data.as_slice(),
+                        ).await;
+
+                        if let Err(e) = status {
+                            log::error!("Error sending data: {}", e);
+                        }
                     }
                 }
-                other => {
-                    let buffer: UInt8Array = data.to_data().into();
+            },
+            Event::Stop => {
+                let mut shared = shared.lock().unwrap();
+                shared.running = false;
 
-                    println!("data received from dataflow {}", other);
-
-                    socket.send(buffer.values()).await.expect("TODO: panic message");
-                }
+                break;
             },
             _ => {}
         }
     }
 
-    listener.await;
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+    let mut shared = shared.lock().unwrap();
+    shared.running = false;
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let (mut node, mut events) = DoraNode::init_from_env()?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::new().init().unwrap();
 
-    pollster::block_on(run(&mut node, &mut events));
+    log::info!("Starting Dora Node Electrode");
 
-    Ok(())
+    let shared = Arc::new(Mutex::new(Shared {
+        running: true,
+        packets: VecDeque::new()
+    }));
+
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+
+    let listener_task = runtime.spawn(run_listener(shared.clone()));
+    let main_task = runtime.spawn(run(shared.clone()));
+
+    let listener = runtime.block_on(listener_task);
+    let main = runtime.block_on(main_task);
+
+    log::info!("Dora Node Electrode stopped");
+
+    return match (listener, main) {
+        (Ok(_), Ok(_)) => Ok(()),
+        (Err(e), _) | (_, Err(e)) => Err(e.into()),
+    };
 }
